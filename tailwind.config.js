5707c35b740c3d4e9f34e684d1d7a0fb678b1825
PranaliChatap <86524033+PranaliChatap@users.noreply.github.com>
2021-10-16 15:22:37 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        pannu:         
+{
+  50: '#ffffda',
+  100: '#ffffad',
+  200: '#ffff7d',
+  300: '#ffff4b',
+  400: '#ffff1a',
+  500: '#e5e600',
+  600: '#b2b300',
+  700: '#7f8000',
+  800: '#4c4d00',
+  900: '#191b00',
+},
+navCol:
+
+
+{
+  50: '#f5eef9',
+  100: '#dbd0e1',
+  200: '#2B3148',
+  300: '#aa93b7',
+  400: '#9174a2',
+  500: '#775a89',
+  600: '#5d466b',
+  700: '#42334c',
+  800: '#281e2f',
+  900: '#0f0914',
+}
+        
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
